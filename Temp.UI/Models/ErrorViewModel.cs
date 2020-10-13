7b0f56aa937eb83f66d01d27aad274ef74360c19
drunkking <dusan.kuburic@outlook.com>
2020-10-13 14:26:10 +0200

    Add project files.

---
+++ Temp.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Temp.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
