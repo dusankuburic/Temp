bfc5da08154f2b714112bc820bb78d56dcbddaf8
dusankuburic <dusan.kuburic@outlook.com>
2020-10-23 10:30:51 +0200

    Added Employee model

---
+++ Temp.Domain/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Temp.Domain.Models
+{
+    public class Employee
+    {
+        public int Id {get; set;}
+        public string FirstName {get; set;}
+        public string LastName {get; set;}
+
+        public ICollection<User> Users {get; set; }
+        public ICollection<Admin> Admins {get; set;}
+    }
+}
