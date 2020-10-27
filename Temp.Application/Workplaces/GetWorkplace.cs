ec73d5fdfc35201358b7c955a45f7603faa1ccf5
dusankuburic <dusan.kuburic@outlook.com>
2020-10-27 21:33:58 +0100

    create/show workplace

---
+++ Temp.Application/Workplaces/GetWorkplace.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Temp.Database;
+
+namespace Temp.Application.Workplaces
+{
+    public class GetWorkplace
+    {
+        private ApplicationDbContext _ctx;
+
+        public GetWorkplace(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public WorkplaceViewModel Do(int id) =>
+            _ctx.Workplaces.Where(x => x.Id == id).Select(x => new WorkplaceViewModel{ 
+                
+            })
+            .FirstOrDefault();
+
+        public class WorkplaceViewModel
+        {
+            public int Id {get; set;}
+            public string Name {get; set;}
+        }
+    }
+}
---
+++ Temp.UI/Controllers/WorkplaceController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Temp.Application.Workplaces;
+using Temp.Database;
+
+namespace Temp.UI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class WorkplaceController : Controller
+    {
+        public readonly ApplicationDbContext _ctx;
+
+        public WorkplaceController(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            return View(new GetWorkplaces(_ctx).Do());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateWorkplace.Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await new CreateWorkplace(_ctx).Do(request);
+
+                if(response.Status)
+                {
+                    TempData["success_message"] = response.Message;
+                    return RedirectToAction("Create");
+                }
+            }
+            return RedirectToAction("Create");
+        }
+    }
+}
