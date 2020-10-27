6b2c808ad3a231e7445251e39cd0aa7d5a4da3de
dusankuburic <dusan.kuburic@outlook.com>
2020-10-27 23:30:57 +0100

    create Emp status

--- Temp.UI/Controllers/EmploymentStatusController.cs
+++ Temp.UI/Controllers/EmploymentStatusController.cs
@@ -3,14 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Temp.Application.EmploymentStatuses;
+using Temp.Database;
 
 namespace Temp.UI.Controllers
 {
     public class EmploymentStatusController : Controller
     {
+
+        private readonly ApplicationDbContext _ctx;
+
+        public EmploymentStatusController(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateEmploymentStatus.Request request)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await new CreateEmploymentStatus(_ctx).Do(request); 
+                
+                if(response.Status)
+                {
+                    TempData["success_message"] = response.Message;
+                    return RedirectToAction("Create");
+                }
+            }
+
+            return RedirectToAction("Create");
+        }
+
+
     }
 }
