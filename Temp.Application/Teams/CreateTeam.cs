fa6ecaf20798d3f72ce5ff4ce26095745219609b
dusan <dusan.kuburic@outlook.com>
2020-12-10 00:18:27 +0100

    create team

--- Temp.Application/Teams/CreateTeam.cs
+++ Temp.Application/Teams/CreateTeam.cs
@@ -1,13 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
+using Temp.Database;
+using Temp.Domain.Models;
 
 namespace Temp.Application.Teams
 {
     public class CreateTeam
     {
-        
+        private readonly ApplicationDbContext _ctx;
+
+        public CreateTeam(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        private async Task<bool> TeamExists(string name, int groupId)
+        {
+            if(await _ctx.Teams.AnyAsync(x => x.Name == name && x.GroupId == groupId))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<Response> Do(Request request)
+        {
+            var teamExists = await TeamExists(request.Name, request.GroupId);
+
+            if(teamExists)
+            {
+                return new Response
+                {
+                    Message = $"Error {request.Name} already exists",
+                    Status = false
+                };
+            }
+
+            var team = new Team
+            {
+                Name = request.Name,
+                GroupId = request.GroupId
+            };
+
+            //Validate
+
+            _ctx.Teams.Add(team);
+            await _ctx.SaveChangesAsync();
+
+            return new Response
+            {
+                Message = $"Success {request.Name} is added",
+                Status = true
+            };
+        }
+
+
+        public class Request
+        {
+            [Required]
+            public int Id { get; set; }
+
+            [Required]
+            public int GroupId { get; set; }
+
+            [Required]
+            [MinLength(2)]
+            [MaxLength(50)]
+            public string Name { get; set; }
+        }
+
+        public class Response
+        {
+            public string Message { get; set; }
+            public bool Status { get; set; }
+        }
     }
 }
--- Temp.UI/Controllers/TeamController.cs
+++ Temp.UI/Controllers/TeamController.cs
@@ -3,14 +3,47 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Temp.Application.Groups;
+using Temp.Application.Teams;
+using Temp.Database;
 
 namespace Temp.UI.Controllers
 {
     public class TeamController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _ctx;
+
+        public TeamController(ApplicationDbContext ctx)
         {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public IActionResult Create(int id)
+        {
+            TempData["group"] = new GetGroup(_ctx).Do(id);
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateTeam.Request request)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await new CreateTeam(_ctx).Do(request);
+
+                if(response.Status)
+                {
+                    TempData["success_message"] = response.Message;
+                }
+                else
+                {
+                    TempData["message"] = response.Message;
+                }
+
+                return RedirectToAction("Create");
+            }
+            return RedirectToAction("Create");
         }
     }
 }
