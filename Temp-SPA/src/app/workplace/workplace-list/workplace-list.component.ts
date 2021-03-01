8094a86f1d9d37391da8e937090c41adf6e1ef5c
dusan <dusan.kuburic@outlook.com>
2021-03-01 21:57:56 +0100

    workplaces spa paging

--- Temp-SPA/src/app/workplace/workplace-list/workplace-list.component.ts
+++ Temp-SPA/src/app/workplace/workplace-list/workplace-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { PaginatedResult, Pagination } from 'src/app/_models/pagination';
 import { Workplace } from 'src/app/_models/workplace';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { WorkplaceService } from 'src/app/_services/workplace.service';
 
 @Component({
   selector: 'app-workplace-list',
@@ -9,13 +12,33 @@ import { Workplace } from 'src/app/_models/workplace';
 })
 export class WorkplaceListComponent implements OnInit {
   workplaces: Workplace[];
+  pagination: Pagination;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private workplaceService: WorkplaceService,
+    private alertify: AlertifyService) { }
 
   ngOnInit(): void {
     this.route.data.subscribe(data => {
-      this.workplaces = data['workplaces'];
+      this.workplaces = data['workplaces'].result;
+      this.pagination = data['workplaces'].pagination;
     });
+  }
+
+  loadWorkplaces(): void {
+    this.workplaceService.getWorkplaces(this.pagination.currentPage, this.pagination.itemsPerPage)
+      .subscribe((res: PaginatedResult<Workplace[]>) => {
+        this.workplaces = res.result;
+        this.pagination = res.pagination;
+      }, error => {
+        this.alertify.error(error.error);
+      })
+  }
+
+  pageChanged(event: any): void {
+    this.pagination.currentPage = event.page;
+    this.loadWorkplaces();
   }
 
 
