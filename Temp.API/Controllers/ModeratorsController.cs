e01edb92fa38faa4c23698ee0884312c9e6225f4
dusan <dusan.kuburic@outlook.com>
2021-03-03 20:18:36 +0100

    register moderator api

---
+++ Temp.API/Controllers/ModeratorsController.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Temp.Application.Auth.Moderators;
+using Temp.Database;
+
+namespace Temp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModeratorsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _ctx;
+        private readonly IConfiguration _config;
+
+        public ModeratorsController(ApplicationDbContext ctx, IConfiguration config)
+        {
+            _ctx = ctx;
+            _config = config;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAdmin(RegisterModerator.Request request)
+        {
+            var response = await new RegisterModerator(_ctx).Do(request);
+            if (response.Status)
+            {
+                return Ok(response);
+            }
+
+            return BadRequest(response.Message);
+        }
+    }
+}
