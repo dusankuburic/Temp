e0c68cb600d6762ebb4a4a9dee599d931f7b0387
dusan <dusan.kuburic@outlook.com>
2021-03-21 13:54:04 +0100

    application custom exceptions

---
+++ Temp.Domain/Models/Applications/Exceptions/ApplicationServiceException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Temp.Domain.Models.Applications.Exceptions
+{
+    public class ApplicationServiceException: Exception
+    {
+        public ApplicationServiceException(Exception innerException)
+            : base("Service error, contact support", innerException)
+        {
+
+        }
+    }
+}
