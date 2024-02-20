2d2c42cc669cfde45eefefc3aa6d921eb3abe230
Dusan Kuburic <dusan.kuburic@outlook.com>
2024-02-20 16:40:10 +0100

    mini bootstrap

---
+++ Temp.API/Bootstrap/ProgramAuthSetup.cs
@@ -0,0 +1,31 @@
+﻿namespace Temp.API.Bootstrap;
+
+public static class ProgramAuthSetup
+{
+    public static IServiceCollection AddAuthSetup(this IServiceCollection services, IConfiguration configuration) {
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options => {
+                options.TokenValidationParameters = new TokenValidationParameters {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.ASCII.GetBytes(configuration.GetSection("AppSettings:Token").Value)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+        services.AddAuthorization(config => {
+            config.AddPolicy("Admin", policyBuilder =>
+                policyBuilder.RequireClaim(ClaimTypes.Role, "Admin"));
+
+            config.AddPolicy("User", policyBuilder =>
+                policyBuilder.RequireClaim(ClaimTypes.Role, "User"));
+
+            config.AddPolicy("Moderator", policyBuilder =>
+                policyBuilder.RequireClaim(ClaimTypes.Role, "Moderator"));
+        });
+
+        return services;
+    }
+}
--- Temp.API/Program.cs
+++ Temp.API/Program.cs
@@ -1,57 +1,19 @@
 using FluentValidation.AspNetCore;
-using Temp.Services.Applications;
-using Temp.Services.Applications.Mappings;
+using Temp.API.Bootstrap;
 using Temp.Services.Applications.Models.Validators;
-using Temp.Services.Employees;
-using Temp.Services.Employees.Mappings;
-using Temp.Services.EmploymentStatuses;
-using Temp.Services.EmploymentStatuses.Mappings;
-using Temp.Services.Engagements;
-using Temp.Services.Engagements.Mappings;
-using Temp.Services.Groups;
-using Temp.Services.Groups.Mapping;
-using Temp.Services.Organizations;
-using Temp.Services.Organizations.Mapping;
-using Temp.Services.Teams;
-using Temp.Services.Teams.Mappings;
-using Temp.Services.Workplaces;
-using Temp.Services.Workplaces.Mappings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAutoMapper(cfg => {
-    cfg.AddProfile<ApplicationsMappingProfile>();
-    cfg.AddProfile<EmployeesMappingProfile>();
-    cfg.AddProfile<EmploymentStatusesMappingProfile>();
-    cfg.AddProfile<EngagementsMappingProfile>();
-    cfg.AddProfile<GroupsMappingProfile>();
-    cfg.AddProfile<OrganizationsMappingProfile>();
-    cfg.AddProfile<TeamsMappingProfile>();
-    cfg.AddProfile<WorkpacesMappingProfile>();
-});
+builder.Services.AddMappingsCollection();
 
-builder.Services.AddScoped<IEmploymentStatusService, EmploymentStatusService>();
-builder.Services.AddScoped<IEngagementService, EngagementService>();
-builder.Services.AddScoped<IGroupService, GroupService>();
-builder.Services.AddScoped<IWorkplaceService, WorkplaceService>();
-builder.Services.AddScoped<IEmployeeService, EmployeeService>();
-builder.Services.AddScoped<IApplicationService, ApplicationService>();
-builder.Services.AddScoped<IOrganizationService, OrganizationService>();
-builder.Services.AddScoped<ITeamService, TeamService>();
+builder.Services.AddProgramServices();
 
-builder.Services.AddCors(opt => {
-    opt.AddPolicy("CorsPolicy", policy => {
-        policy
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .WithOrigins("http://localhost:4200");
-    });
-});
+builder.Services.ConfigureCORS();
+
 
 builder.Services.AddControllers()
     .AddNewtonsoftJson(opt => opt.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore)
     .AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<CreateApplicationRequestValidator>());
-
 
 
 builder.Services.Configure<ApiBehaviorOptions>(opt => {
@@ -59,27 +21,7 @@ builder.Services.Configure<ApiBehaviorOptions>(opt => {
         new BadRequestObjectResult(ctx.ModelState);
 });
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options => {
-        options.TokenValidationParameters = new TokenValidationParameters {
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(
-                Encoding.ASCII.GetBytes(builder.Configuration.GetSection("AppSettings:Token").Value)),
-            ValidateIssuer = false,
-            ValidateAudience = false
-        };
-    });
-
-builder.Services.AddAuthorization(config => {
-    config.AddPolicy("Admin", policyBuilder =>
-        policyBuilder.RequireClaim(ClaimTypes.Role, "Admin"));
-
-    config.AddPolicy("User", policyBuilder =>
-        policyBuilder.RequireClaim(ClaimTypes.Role, "User"));
-
-    config.AddPolicy("Moderator", policyBuilder =>
-        policyBuilder.RequireClaim(ClaimTypes.Role, "Moderator"));
-});
+builder.Services.AddAuthSetup(builder.Configuration);
 
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
